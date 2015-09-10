5633b79355ff65532f3fb5f41d569986b0a985bd
Nicolas Sceaux <nicolas.sceaux@thalesgroup.com>
2015-09-10 05:03:21 +0200

    Fix .rio grammar

--- src/test/java/com/thalesgroup/rtrtunit/riochecker/SyntaxRioCheckerTest.java
+++ src/test/java/com/thalesgroup/rtrtunit/riochecker/SyntaxRioCheckerTest.java
@@ -214,6 +214,11 @@ public class SyntaxRioCheckerTest {
     }
 
     @Test
+    public void testValidation21() throws Exception {
+        testChecker("out21.rio");
+    }
+
+    @Test
     public void testValidationA350rtrt7() throws Exception {
     	testChecker("a350_rtrt7.rio");
     }
